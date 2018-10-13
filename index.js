cbe78d54cc605d9751cc227c7eee29f86877ef46
Andrew Bell <andrew@recursivechaos.com>
2018-10-13 14:10:01 -0500

    WIP - server info

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const server = require('./src/server');
 async function start() {
   try {
     await server.start();
+    console.log(server.info);
     console.log(`Started server on ${ process.env.PORT }`);
   } catch (e) {
     console.error(e);
